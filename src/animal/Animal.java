e92d57516d6cf9a490e0848367963efce9423ce2
gheorghe835 <gheorghe835@gmail.com>
2024-12-20 20:48:57 +0200

    adaugarea clasa animal(restart)

--- src/animal/Animal.java
+++ src/animal/Animal.java
@@ -1,5 +1,18 @@
 package animal;
 
+/*Creează o clasă Animal cu atribute: nume (String) și sunet (String).
+Creează o metodă care afișează sunetul făcut de animal într-un format precum: „[nume] face [sunet]”. */
+
 public class Animal {
+    String animal;
+    String sound;
+    public Animal(String animal, String sound) {
+        this.animal = animal;
+        this.sound = sound;
+    }
+
+    public void makeSound(){
+        System.out.println(animal + " face " + sound);
+    }
 
 }
