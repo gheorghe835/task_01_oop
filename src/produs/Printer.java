94c0d5f948b1dbfac2c2d4be17b99c567d946e69
gheorghe835 <gheorghe835@gmail.com>
2024-12-20 20:25:41 +0200

    adaugarea produsului laptop

--- src/produs/Printer.java
+++ src/produs/Printer.java
@@ -1,4 +1,12 @@
 package produs;
 
 public class Printer {
+    public static void main(String[] args) {
+        Produs laptop = new Produs("Lenova",27000.45,78);
+
+        laptop.showDetails();
+        laptop.upStock(4);
+        laptop.addDiscount(10);
+        laptop.showDetails();
+    }
 }
