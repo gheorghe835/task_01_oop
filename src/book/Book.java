4d286f35b5dda66e029fc3608f125c71446b3f89
gheorghe835 <gheorghe835@gmail.com>
2024-12-20 08:19:54 +0200

    adaugarea conditiei la taskul cu cartea

--- src/book/Book.java
+++ src/book/Book.java
@@ -1,4 +1,8 @@
 package book;
+/*Clasa Carte
+Creează o clasă numită Carte cu următoarele atribute: titlu (String), autor (String), numarPagini (int).
+Adaugă un constructor pentru inițializarea atributelor.
+Creează o metodă care verifică dacă cartea are mai mult de 300 de pagini. */
 
 public class Book {
     String title ;
