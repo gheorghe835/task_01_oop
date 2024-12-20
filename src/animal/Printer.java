cef396c48399d90614f201592a12cd31643ea708
gheorghe835 <gheorghe835@gmail.com>
2024-12-20 20:46:51 +0200

    adaugarea tascului cu clasa animal si metodei makeSound plus animalele

---
+++ src/animal/Printer.java
@@ -0,0 +1,8 @@
+package animal;
+
+public class Printer {
+    public static void main(String[] args) {
+        Animal ciine = new Animal("  Ciinele"," ham ham");
+        Animal pisica = new Animal("  Motanul ", " miau miau");
+    }
+}
