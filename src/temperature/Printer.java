3c0e48b425f3e6a75175e2d89ec1aa72f330f507
gheorghe835 <gheorghe835@gmail.com>
2024-12-21 06:56:55 +0200

    afisasrea datelor despre temperatura

--- src/temperature/Printer.java
+++ src/temperature/Printer.java
@@ -1,4 +1,9 @@
 package temperature;
 
 public class Printer {
+    public static void main(String[] args) {
+        Temperature temperature = new Temperature(43.2);
+        System.out.println("  Temperatura: " + temperature.getValue() + " - in celsius sunt: " + temperature.fahrenheitToCelcius() + 
+         ", in fahreinheit sunt: " + temperature.celciusToFahrenheit());
+    }
 }
