b2d5b9645a0ba940355b5e943fb5a46ac0885a0f
gheorghe835 <gheorghe835@gmail.com>
2024-12-21 07:49:46 +0200

    adaugarea datelor despre un joc video

--- src/videoGame/Printer.java
+++ src/videoGame/Printer.java
@@ -1,4 +1,22 @@
 package videoGame;
 
 public class Printer {
+    public static void main(String[] args) {
+        VideoGame videoGame = new VideoGame("Joc","Actiune", 4.1);
+
+        // Afișarea detaliilor despre joc
+        videoGame.showDetails();
+
+        // Verificarea popularității
+        if (videoGame.isPopular()) {
+            System.out.println(videoGame.getName() + " -- este popular");
+        }
+        else{
+            System.out.println(videoGame.getName() + " -- nu este popular");
+        }
+        // Schimbarea genului
+        videoGame.changeGenre("Aventura");
+        videoGame.showDetails();
+
+    }
 }
