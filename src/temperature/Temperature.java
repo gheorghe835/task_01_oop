bf50a729f2b8e0d55da31016269f48bfdcba1947
gheorghe835 <gheorghe835@gmail.com>
2024-12-21 06:48:07 +0200

    adaugarea metodelor de conversii in celsius si in fahrenheit in clasa Temperature

--- src/temperature/Temperature.java
+++ src/temperature/Temperature.java
@@ -1,4 +1,23 @@
 package temperature;
 
-public class Temperature {
+public class Temperature { 
+    private double value;
+    public Temperature(double value){
+        this.value = value;
+    }
+    public double getValue(){
+        return value;
+    }
+    public void setValue(double value){
+        this.value = value;
+    }
+
+    // Metodă pentru a converti temperatura din Celsius în Fahrenheit
+    public double celciusToFahrenheit(){
+        return (value * 9 / 5) + 32;
+    }
+    // Metodă pentru a converti temperatura din Fahrenheit în Celsius
+    public double fahrenheitToCelcius(){
+        return (value - 32) * 5 / 9;
+    }
 }
