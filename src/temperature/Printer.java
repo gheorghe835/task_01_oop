4f891d86f4fcb5060d4ed8f47612882633dd23d8
gheorghe835 <gheorghe835@gmail.com>
2024-12-21 07:02:47 +0200

    afisasrea datelor despre temperatura celsius si fahrenheit

--- src/temperature/Printer.java
+++ src/temperature/Printer.java
@@ -5,5 +5,10 @@ public class Printer {
         Temperature temperature = new Temperature(43.2);
         System.out.println("  Temperatura: " + temperature.getValue() + " - in celsius sunt: " + temperature.fahrenheitToCelcius() + 
          ", in fahreinheit sunt: " + temperature.celciusToFahrenheit());
+        temperature.setValue(33);
+        System.out.println("  Temperatura in celsius: " + temperature.getValue() + " - in fahrenheit sunt: " + 
+        temperature.celciusToFahrenheit()); 
+        System.out.println("  Temperatura in fahrenheit: " + temperature.getValue() + " - in celsius sunt: " + 
+        temperature.fahrenheitToCelcius()); 
     }
 }
