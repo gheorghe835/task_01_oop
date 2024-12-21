48be1d10f1443e8986860d06f19f08a0d4b3a48e
gheorghe835 <gheorghe835@gmail.com>
2024-12-21 07:42:42 +0200

    adaugarea clasei VideoGame cu metodele: isPopular(), changeGenre(),showDetails()

--- src/videoGame/VideoGame.java
+++ src/videoGame/VideoGame.java
@@ -1,4 +1,39 @@
 package videoGame;
+/*Creează o clasă JocVideo cu atribute: nume (String), gen (String), rating (double).
+Creează o metodă care verifică dacă ratingul jocului este peste 4.0.
+Adaugă o metodă care schimbă genul jocului. */
 
 public class VideoGame {
+    private String name;
+    private String genre;
+    private double rating;
+
+    public VideoGame( String name, String genre, double rating){
+        this.name = name;
+        this.genre = genre;
+        this.rating = rating;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getGenre(){
+        return genre;
+    }
+    public double getRating(){
+        return rating;
+    }
+    
+    public boolean isPopular(){
+        return rating > 0;
+    }
+    // Metodă pentru a verifica dacă rating-ul este peste 4.0
+    public void changeGenre(String newGenre){
+        genre = newGenre;
+        System.out.println("  genul jocului a fost shimbat la: " + newGenre);
+    }
+    // Metodă pentru a afișa detalii despre joc
+    public void showDetails(){
+        System.out.println(" Joc:" + name + "\n Gen: " + genre + "\n Rating: " + rating);
+    }
 }
